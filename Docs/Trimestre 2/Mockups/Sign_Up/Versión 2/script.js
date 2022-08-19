560f33529d07e719fe0db76da678eabeab6f4c51
AnibalSJ <101675963+AnibalSJ@users.noreply.github.com>
2022-08-18 20:02:55 -0500

    Delete Docs/Trimestre 2/Mockups/Sign_Up/Versión 2 directory

--- Docs/Trimestre 2/Mockups/Sign_Up/Versión 2/script.js
+++
@@ -1,6 +0,0 @@
-let form = document.querySelector('.form');
-    let tapa = document.querySelector('.tapa');
-    tapa.addEventListener('click', ()=>{
-        tapa.classList.toggle('movimiento-tapa');
-        form.classList.toggle('form-movimiento');
-    });
