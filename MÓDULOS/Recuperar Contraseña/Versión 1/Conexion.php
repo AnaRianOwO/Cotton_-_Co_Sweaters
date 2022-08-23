40a4e3548367e918a0ebbd5903aa067cb4163a03
CarlosDaniel99 <101676004+CarlDaniel99@users.noreply.github.com>
2022-08-23 17:37:28 -0500

    Add files via upload

---
+++ MÓDULOS/Recuperar Contraseña/Versión 1/Conexion.php
@@ -0,0 +1,8 @@
+<?php
+$conn = new mysqli("localhost","root","","loginvaidroll4");
+	
+	if($conn->connect_errno)
+	{
+		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
+	}
+?>
