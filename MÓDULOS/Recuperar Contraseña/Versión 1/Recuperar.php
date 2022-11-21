1a6a40c7b959dcb7fcd2e9f04c6c894337271885
AnaRianOwO <amriano6@misena.edu.co>
2022-11-21 17:01:10 -0500

    arreglo de la rama

--- MÓDULOS/Recuperar Contraseña/Versión 1/Recuperar.php
+++ MÓDULOS/Recuperar Contraseña/Versión 1/Recuperar.php
@@ -14,22 +14,7 @@ $paracorreo 		= $correo;
 $titulo				= "Recuperar contraseña";
 $mensaje			= $enviarpass;
 $tucorreo			= "From: danielgiraldo.coper@gmail.com";
-<?php
-include('conexion.php');
 
-$correo = $_POST['txtcorreo'];
-
-$queryusuario 	= mysqli_query($conn,"SELECT * FROM login WHERE correo = '$correo'");
-$nr 			= mysqli_num_rows($queryusuario); 
-if ($nr == 1)
-{
-$mostrar		= mysqli_fetch_array($queryusuario); 
-$enviarpass 	= $mostrar['pass'];
-
-$paracorreo 		= $correo;
-$titulo				= "Recuperar contraseña";
-$mensaje			= $enviarpass;
-$tucorreo			= "From: danielgiraldo.coper@gmail.com";
 
 if(mail($paracorreo,$titulo,$mensaje,$tucorreo))
 {
