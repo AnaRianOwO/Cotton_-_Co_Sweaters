261af3cd50d0e2fe9e99a57d27469bd3161bbc20
Your Name <you@example.com>
2022-11-24 09:38:20 -0500

    correción en la consultas sql de editar venta

--- Código/Ingreso/Dashboard/Factura/Tablas/editar_venta.php
+++ Código/Ingreso/Dashboard/Factura/Tablas/editar_venta.php
@@ -7,13 +7,13 @@ $sql = "SELECT f.idFactura, u.idUsuario, u.firstName, u.secondName, u.surname, u
 $resultadoFactura = mysqli_query($conexion, $sql);
 $factura = mysqli_fetch_assoc($resultadoFactura);
 
-$consulta= "giut ";
+$consulta= "SELECT  df.idFactura, df.codigo, p.nameProducto, p.descripcion, p.precio, df.cantidad FROM detallefactura df INNER JOIN factura f ON f.idFactura = df.idFactura INNER JOIN producto p ON df.codigo = p.codigo WHERE f.idFactura= '$idFactura'";
 $detalles = mysqli_query($conexion, $consulta);
 
 $sqlUsuarios = "SELECT u.idUsuario, u.firstName, u.secondName, u.surname, u.secondSurname, u.phone, u.direccion FROM usuario u;";
 $consultaUsuario = mysqli_query($conexion, $sqlUsuarios);
 
-$sqlProductos = "SELECT p.codigo, p.nameProducto, p.descripcion, p.precio";
+$sqlProductos = "SELECT p.codigo, p.nameProducto, p.descripcion, p.precio FROM producto p";
 $consultaProductos = mysqli_query($conexion, $sqlProductos);
 
 // while($row = $consultaUsuario->fetch_assoc())
@@ -80,6 +80,8 @@ $consultaProductos = mysqli_query($conexion, $sqlProductos);
                                         <?php while($row = mysqli_fetch_array($detalles)){?>
                                         <tr>
                                             <td><input type="text" value="<?php echo $row['codigo'] ?>"></td>
+                                            <td><select name="" id="codigo"></select></td>
+                                            
                                             <td><?php echo $row['nameProducto'] ?></td>
                                             <td><?php echo $row['descripcion'] ?></td>
                                             <td><?php echo $row['precio'] ?></td>
