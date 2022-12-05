af2fc773e52d0e42b61dc75c37f06a0e89dda40b
Your Name <you@example.com>
2022-12-05 17:15:47 -0500

    Actualización de los TyC en el registro, redireccionamiento

--- Código/Registro/index.php
+++ Código/Registro/index.php
@@ -260,10 +260,10 @@ include "registrar.php";
     <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <script src="TyC/funciones.js"></script>
+    <script src="funciones.js"></script>
 </body>
 
-<script src="TyC/funciones.js"></script>
+<script src="funciones.js"></script>
 <script>
     let btn = document.getElementById('btn_registrar');
 
