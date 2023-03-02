94b418d8c50ea2aa14928cefa90e8bcf2a86086a
AnibalSJ <anibalmadera23@gmail.com>
2023-03-02 18:46:48 -0500

    Recuperar contraseña

--- Código/Ingreso/Restablecer/update_password.php
+++ Código/Ingreso/Restablecer/update_password.php
@@ -7,8 +7,27 @@
 
     if($p1 == $p2){
         $p1 = password_hash($p1, PASSWORD_DEFAULT);
-        $DB -> query("UPDATE usuario SET pass = '$p1' WHERE correo = '$correo'") or die($DB -> error);
-        echo "<script>alert('Contraseña actualizada');window.location='../index.php'</script>";
+        $DB -> query("UPDATE persona SET pass = '$p1' WHERE correo = '$correo'") or die($DB -> error);
+        echo "
+            .
+            <html>
+                <script src='https://unpkg.com/sweetalert2@9.5.3/dist/sweetalert2.all.min.js'></script>
+            <html>
+            <script>
+                Swal
+                    .fire({
+                        title: 'Ha ocurrido un error',
+                        text: 'Por favor verifique su información',
+                        icon: 'error',
+                        confirmButtonText: 'Continuar'
+                    })
+                    .then(resultado => {
+                        if (resultado.value) {
+                            window.location='index.php';
+                        }else {    
+                        }
+                    });
+            </script>;window.location='../index.php'</script>";
     }else{
         echo "Las contraseñas no coinciden";
     }
